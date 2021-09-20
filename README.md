4176ee9787e1cb0b4bf49d6aff2d20e91714347a
carlosmdiaz <carlosdiaz3979@gmail.com>
2021-09-20 14:49:49 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# fsm-variables-xynnfj
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/fsm-variables-xynnfj)
---
+++ index.js
@@ -0,0 +1,28 @@
+// Import stylesheets
+import './style.css';
+
+
+let luckyNumber = 10;
+console.log(luckyNumber);
+
+let arrayStudents = ["Ahh", "Melissa", "Huda"];
+console.log(arrayStudents);
+
+console.log(arrayStudents[1]);
+
+
+// TASK
+// Create 2 variables 👇
+// Variable: firstName with value of your firstName 
+// Variable: lastName with value of your lastName
+// Console log it out
+// EXTRA CREDIT: Concatinate firstName and lastName into variable fullName 🔥
+
+let firstName = "Carlos";
+let lastName = "Diaz";
+
+console.log(firstName + " " + lastName);
+
+let fullName = firstName + " " + lastName;
+
+console.log(fullName);
